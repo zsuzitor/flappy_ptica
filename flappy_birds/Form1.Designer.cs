c96a0a10c156bcf4886150a196347d5e630f97e1
stas <avdosevstas@mail.ru>
2017-06-08 18:33:48 +0300

    1 stolb

---
+++ flappy_birds/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace flappy_birds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bird_form = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.s4et = new System.Windows.Forms.Label();
+            this.column2_form = new System.Windows.Forms.PictureBox();
+            this.column1_form = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bird_form)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.column2_form)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.column1_form)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bird_form
+            // 
+            this.bird_form.Location = new System.Drawing.Point(360, 217);
+            this.bird_form.Name = "bird_form";
+            this.bird_form.Size = new System.Drawing.Size(62, 55);
+            this.bird_form.TabIndex = 0;
+            this.bird_form.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.s4et);
+            this.panel1.Controls.Add(this.column2_form);
+            this.panel1.Controls.Add(this.column1_form);
+            this.panel1.Controls.Add(this.bird_form);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(884, 562);
+            this.panel1.TabIndex = 1;
+            // 
+            // s4et
+            // 
+            this.s4et.AutoSize = true;
+            this.s4et.Location = new System.Drawing.Point(13, 13);
+            this.s4et.Name = "s4et";
+            this.s4et.Size = new System.Drawing.Size(13, 13);
+            this.s4et.TabIndex = 3;
+            this.s4et.Text = "0";
+            // 
+            // column2_form
+            // 
+            this.column2_form.Location = new System.Drawing.Point(765, 359);
+            this.column2_form.Name = "column2_form";
+            this.column2_form.Size = new System.Drawing.Size(50, 200);
+            this.column2_form.TabIndex = 2;
+            this.column2_form.TabStop = false;
+            // 
+            // column1_form
+            // 
+            this.column1_form.Location = new System.Drawing.Point(763, 3);
+            this.column1_form.Name = "column1_form";
+            this.column1_form.Size = new System.Drawing.Size(50, 200);
+            this.column1_form.TabIndex = 1;
+            this.column1_form.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 562);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "FB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.bird_form)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.column2_form)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.column1_form)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bird_form;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox column2_form;
+        private System.Windows.Forms.PictureBox column1_form;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label s4et;
+    }
+}
+
---
+++ flappy_birds/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace flappy_birds
+{
+    public partial class Form1 : Form
+    {
+        public List<coll> coll_list = new List<coll>();
+
+
+        int column1_top = 0;
+        int column1_left = 763;
+        int column2_top = 386;
+        int column2_left = 763;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //тик
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool loose_stolkn = true;
+            bird_form.Top += 2;
+            //column1_form
+            dvig_column_otrisovka();
+            //обработать столконовение
+            loose_stolkn=test_stolknovenii();
+
+            if (!loose_stolkn)
+                this.Close();
+
+            // if(bird_form.Top<=column1_form.Bottom&&)
+
+        }
+
+        //чекнуть краш
+        private bool test_stolknovenii()
+        {
+            if (bird_form.Top <= 0)
+                return false;
+            if (bird_form.Bottom >= panel1.Bottom)
+                return  false;
+            if(bird_form.Top<=column1_form.Bottom&& bird_form.Right >= column1_form.Left&& bird_form.Right<= column1_form.Right)
+                return false;
+
+            if (bird_form.Bottom >= column2_form.Top && bird_form.Right >= column2_form.Left && bird_form.Right <= column2_form.Right)
+                return false;
+
+
+            if (bird_form.Right >= column2_form.Right && bird_form.Right <= column2_form.Right + 2)
+            {
+                int a = Convert.ToInt32(s4et.Text);
+                s4et.Text = (++a).ToString();
+            }
+                
+
+                return true;
+        }
+        private void dvig_column_otrisovka()
+        {
+            if(column1_form.Left>0)
+            {
+                column1_form.Left -= 3;
+                column2_form.Left -= 3;
+            }
+            else
+            {
+                //перерисовать
+                column1_form.Left = 763;
+                column2_form.Left = 763;
+                Random rand = new Random();
+                rand.Next(0,10);
+
+
+            }
+                
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+            bird_form.Image = Image.FromFile("bird.jpg");
+            bird_form.SizeMode = PictureBoxSizeMode.StretchImage;
+            
+                column1_form.Image = Image.FromFile("column.PNG");
+            column1_form.SizeMode = PictureBoxSizeMode.StretchImage;
+            column2_form.Image = Image.FromFile("column.PNG");
+            column2_form.SizeMode = PictureBoxSizeMode.StretchImage;
+            for(int i=0;i<3;++i)
+            {
+                coll_list.Add(new coll(column1_form.Top, column1_form.Bottom, column1_form.Left+150*i, column1_form.Right + 150 * i, i+1));
+                PictureBox a = new PictureBox();
+
+            }
+            for (int i = 3; i < 6; ++i)
+            {
+                coll_list.Add(new coll(column1_form.Top + 356, column1_form.Bottom+356, column1_form.Left + 150 * i, column1_form.Right + 150 * i, i+1));
+
+            }
+
+            column1_form.Left = column1_left;
+            column2_form.Left = column2_left;
+            column1_form.Top = column1_top;
+            column2_form.Top = column2_top;
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Up)
+            {
+                bird_form.Top -=60;
+
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+
+            }
+        }
+    }
+
+
+
+    public class coll
+    {
+        public int top;
+        public int bott;
+        public int left;
+        public int right;
+        public int number;
+
+        public coll(int a, int b, int c, int d, int g)
+        {
+            top = a;
+            bott = b;
+            left = c;
+            right = d;
+            number = g;
+        }
+
+
+    }
+}
